d44d36594a5d32de0d48896596e52ef47537a89c
mrpensa <mariopensa97@gmail.com>
2023-11-01 00:53:15 -0300

    navbar

--- app/src/main/java/com/example/bepawsomeandroid/MainActivity.kt
+++ app/src/main/java/com/example/bepawsomeandroid/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.bepawsomeandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import com.example.bepawsomeandroid.Fragment.Home
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+       /* supportFragmentManager.commit{
+            replace<Home>(R.id.frame_container)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }*/
+
+        
     }
 }
