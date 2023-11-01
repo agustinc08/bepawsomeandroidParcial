f448d30fa7ee61d67683f61106ef2435439a3416
mrpensa <mariopensa97@gmail.com>
2023-11-01 01:42:07 -0300

    navbar completo

--- app/src/main/java/com/example/bepawsomeandroid/MainActivity.kt
+++ app/src/main/java/com/example/bepawsomeandroid/MainActivity.kt
@@ -4,13 +4,62 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
-import com.example.bepawsomeandroid.Fragment.Home
+import com.example.bepawsomeandroid.Fragment.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navegation : BottomNavigationView
+
+    private val mOnNavMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        when (item.itemId){
+            R.id.home -> {
+                supportFragmentManager.commit {
+                    replace<Home>(R.id.frame_container)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.favorite -> {
+                supportFragmentManager.commit {
+                    replace<Favorite>(R.id.frame_container)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.adoption -> {
+                supportFragmentManager.commit {
+                    replace<Adoption>(R.id.frame_container)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.publication -> {
+                supportFragmentManager.commit {
+                    replace<Publication>(R.id.frame_container)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        navegation = findViewById(R.id.navMenu)
+        navegation.setOnNavigationItemSelectedListener(mOnNavMenu)
+
        supportFragmentManager.commit{
             replace<Home>(R.id.frame_container)
             setReorderingAllowed(true)
