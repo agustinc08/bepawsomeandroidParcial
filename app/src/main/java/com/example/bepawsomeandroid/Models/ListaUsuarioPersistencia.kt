3a1ac0505e1fe8ee8fe1a99366855d07d4c52c23
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-11-06 20:30:00 -0300

    Se actualiza Usuario con el campo telefono.

--- app/src/main/java/com/example/bepawsomeandroid/Models/ListaUsuarioPersistencia.kt
+++ app/src/main/java/com/example/bepawsomeandroid/Models/ListaUsuarioPersistencia.kt
@@ -4,10 +4,10 @@ class ListaUsuariosPersistencia {
     val usuariosList: MutableList<Usuario> = mutableListOf()
 
     init {
-        usuariosList.add(Usuario("Admin", "admin.gmail@gmail.com","default","Admin" ))
-        usuariosList.add(Usuario("Tester", "tester.gmail@gmail.com","default","Tester" ))
-        usuariosList.add(Usuario("Pepe", "pepe.gmail@gmail.com","default","Pepe" ))
-        usuariosList.add(Usuario("Pablo", "palbo.gmail@gmail.com","default","Admin" ))
+        usuariosList.add(Usuario("Admin", "admin.gmail@gmail.com","default","Admin",  "123456" ))
+        usuariosList.add(Usuario("Tester", "tester.gmail@gmail.com","default","Tester",  "234567" ))
+        usuariosList.add(Usuario("Pepe", "pepe.gmail@gmail.com","default","Pepe", "345678" ))
+        usuariosList.add(Usuario("Pablo", "palbo.gmail@gmail.com","default","Admin","456789" ))
     }
 
     fun encontrarUsuario(nombre: String, password: String): Usuario?{
--- app/src/main/java/com/example/bepawsomeandroid/Models/Usuario.kt
+++ app/src/main/java/com/example/bepawsomeandroid/Models/Usuario.kt
@@ -1,4 +1,4 @@
 package com.example.bepawsomeandroid.Models
 
-class Usuario (val nombre: String, val mail: String, val foto: String, val password: String){
+class Usuario (val nombre: String, val mail: String, val foto: String, val password: String, val telefono: String){
 }
