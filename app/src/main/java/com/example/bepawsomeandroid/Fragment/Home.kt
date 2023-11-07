aadbbab8660a4b188372892115b460b4b8c7a0ed
mrpensa <mariopensa97@gmail.com>
2023-11-07 00:11:39 -0300

    boton de favoritos

--- app/src/main/java/com/example/bepawsomeandroid/Fragment/Home.kt
+++ app/src/main/java/com/example/bepawsomeandroid/Fragment/Home.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -27,6 +29,7 @@ class Home : Fragment() {
     }
 
     private lateinit var animalButtonsLayout: LinearLayout
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -62,8 +65,8 @@ class Home : Fragment() {
         val breedTextView: TextView = customView.findViewById(R.id.animalBreedTextView)
         val ageTextView: TextView = customView.findViewById(R.id.animalAgeTextView)
         val sexTextView: TextView = customView.findViewById(R.id.animalSexTextView)
+        val likeButton = customView.findViewById<Button>(R.id.likeButton)
         val imageView = customView.findViewById<ImageView>(R.id.animalImageView)
-
 
         nameTextView.text = "Nombre: ${animal.nombre}"
         breedTextView.text = "Raza: ${animal.raza}"
@@ -74,13 +77,23 @@ class Home : Fragment() {
             .load(animal.imgUrl1)
             .into(imageView)
 
-        customView.setOnClickListener {
-       
-            val intent = Intent(requireContext(), DataAnimalActivity::class.java)
+        var isLiked = false  // Variable para rastrear si se ha presionado "Me gusta"
 
-            startActivity(intent)
+        likeButton.setOnClickListener {
+            if (isLiked) {
+                likeButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.purple_700))
+                isLiked = false
+            } else {
+                likeButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.blue_menu))
+                //aca es donde lo guardo en la lista de fav
+                isLiked = true
+            }
         }
 
+        customView.setOnClickListener {
+            val intent = Intent(requireContext(), DataAnimalActivity::class.java)
+            startActivity(intent)
+        }
 
         return customView
     }
