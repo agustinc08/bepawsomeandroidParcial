b9d7ef6ee437740f71bf16d705245d3e2747e8ef
ALDO SEBASTIAN LOPEZ <asldaras@gmail.com>
2023-11-07 19:51:49 -0300

    Home con Usuario Mejorado

--- app/src/main/java/com/example/bepawsomeandroid/Fragment/Home.kt
+++ app/src/main/java/com/example/bepawsomeandroid/Fragment/Home.kt
@@ -1,5 +1,6 @@
 package com.example.bepawsomeandroid.Fragment
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,14 +14,23 @@ import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.bepawsomeandroid.Activity.DataAnimalActivity
 import com.example.bepawsomeandroid.Models.Animal
+import com.example.bepawsomeandroid.Models.User
 import com.example.bepawsomeandroid.R
 import com.example.bepawsomeandroid.ViewModels.AnimalViewModel
 import com.example.bepawsomeandroid.ViewModels.AnimalViewModelFactory
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import org.json.JSONException
+import org.json.JSONObject
 
 class Home : Fragment() {
+
+    lateinit var nameUserCredential: String
+    lateinit var imageUrlUserCredential: String
+
 
     private val animalViewModel: AnimalViewModel by lazy {
         ViewModelProvider(this, AnimalViewModelFactory()).get(AnimalViewModel::class.java)
@@ -30,12 +40,41 @@ class Home : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val sharedPreferences = requireContext().getSharedPreferences("Credenciales", Context.MODE_PRIVATE)
+
+        val jsonObjectString = sharedPreferences.getString("UserLogueado", null)
+        var jsonObject: JSONObject? = null
+        if (jsonObjectString != null) {
+            try {
+                jsonObject = JSONObject(jsonObjectString)
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+        var gson = Gson()
+        var userObject = gson.fromJson(jsonObject.toString(), User::class.java)
+        nameUserCredential = userObject.name
+        imageUrlUserCredential = userObject.imageUrl
+
+        val textViewUserName = view.findViewById<TextView>(R.id.textViewNombreUsuario)
+        textViewUserName.text = nameUserCredential
+
+        val imageViewUser = view.findViewById<ImageView>(R.id.imageViewUserProfile2)
+        if (imageUrlUserCredential.isNotEmpty()) {
+            Picasso.get().load(imageUrlUserCredential).into(imageViewUser)
+        }
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         animalButtonsLayout = view.findViewById(R.id.animalButtonsLayout)
 
@@ -56,6 +95,8 @@ class Home : Fragment() {
             }
         })
     }
+
+
 
     private fun createCustomAnimalView(animalId: String, animal: Animal): View {
         val customView = layoutInflater.inflate(R.layout.custom_animal_view, null)
